072972c3a48c5b5f663878d05042ba13f6be65d7
zharui <zharui@semidata.com>
2014-05-12 11:45:46 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sdm003::Application.config.secret_key_base = 'a4555cc5e77c89c5ff68068bc74f097b1f7e3bafcac01485729993f1296b6bb25c0705e493c65787a3e6436472e9b430ac9a583f7c3ed3cc8e7ffc7dfcf0bba6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sdm003::Application.config.session_store :cookie_store, key: '_sdm-0_0_3_session'
